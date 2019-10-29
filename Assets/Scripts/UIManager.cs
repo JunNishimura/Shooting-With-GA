494dbae30f19efdebe928d6129526948fd53846a
JunNishimura <n.junjun0303@gmail.com>
2019-10-29 23:32:41 +0900

    Add UIManager which manages the secene UI.
    I wanted to manage all UI from one script because it is clean and simple.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private float prevCurGeneration;
+    public Text PopSizeText;
+    public Text MutateProbText;
+    public Text CurGenerationText;
+    public Text BestFitnessText;
+    public Text SurvivedCountText;
+
+    private void Awake()
+    {
+        UIUpdate();
+        prevCurGeneration = Simulation.curGeneration;
+    }
+
+    private void Update() 
+    {
+        // an increment of curGeneration is a flag to change the UI
+        if (prevCurGeneration != Simulation.curGeneration) 
+        {
+            UIUpdate();
+            prevCurGeneration = Simulation.curGeneration;
+        }
+    }
+
+    private void UIUpdate() 
+    {
+        PopSizeText.text       = $">> {Simulation.BulletNum}";
+        MutateProbText.text    = $">> {Population.MUTATEPROB}";
+        CurGenerationText.text = $">> {Simulation.curGeneration}";
+        BestFitnessText.text   = $">> {Simulation.BestFitnessEver:N5}";
+        SurvivedCountText.text = $">> {Simulation.SurvivedCount}";
+    }
+}
