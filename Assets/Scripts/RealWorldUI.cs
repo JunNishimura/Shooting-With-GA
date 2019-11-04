5bfd6d927bdb97645ba71169a6f9f4636f723958
JunNishimura <n.junjun0303@gmail.com>
2019-11-04 20:27:05 +0900

    Add RealWorldUI.cs for the manipulation of fitness text
    I was controling fitness text from the Simulation.cs, but I do not want Simulation.cs to manipulate UI.
    That is why I separated Simulation.cs into two scripts.
    I need to add explanation and rename a bit.

---
+++ Assets/Scripts/RealWorldUI.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RealWorldUI : MonoBehaviour
+{
+    public Text TerminalText;
+    public GameObject textObject;
+
+    // interval to dispaly next character
+    [SerializeField][Range(0.001f, 0.3f)] private float charDisplayInterval = 0.05f; 
+    private string[] fitnessSentences;
+    private string wholeSentence;
+    private string nowSentence;
+    public int writingNum { get; private set; }
+    private int setNum;
+    private float beginTime;
+    private float displayLength;
+    private int lastCharIndex;
+    private bool isTextScrollUp;
+    private bool isWriting;
+    private int fontSize;
+    private int maxLinesNumOnScreen; // maximum number of lines which the scene can display
+
+    private void Awake() 
+    {
+        fitnessSentences = new string[Simulation.BulletNum];
+        fontSize = TerminalText.fontSize;
+        maxLinesNumOnScreen = (int)(Screen.height / fontSize);
+    }
+
+    private void Update() 
+    {
+        if (isWriting && writingNum < setNum) 
+        {
+            // scroll up when the text is full on the screen
+            if (isTextScrollUp) 
+            {
+                // calculate how much the text should be scrolled per frame
+                float scroll = (Time.deltaTime * fontSize) / displayLength; 
+                // scroll *= 1.005f; // an adjustment
+                textObject.GetComponent<RectTransform>().localPosition += Vector3.up * scroll;
+            }
+
+            if (Time.time > beginTime + displayLength)
+            {
+                wholeSentence += nowSentence + "\n";
+                DisplayTerminalText(wholeSentence);
+                writingNum++;
+
+                if (writingNum < setNum) 
+                {
+                    SetNextSentence(fitnessSentences, writingNum+1);
+                }
+                else 
+                {
+                    isWriting = false;
+                }
+            }
+            else 
+            {
+                // here is just visiting visiting nowSentence one by one
+                int nowCharIndex = (int)(Mathf.Clamp01((Time.time-beginTime) / displayLength) * nowSentence.Length);
+                // if index has changed, show it
+                if (nowCharIndex != lastCharIndex)
+                {
+                    string partialSentence = wholeSentence + nowSentence.Substring(0, nowCharIndex);
+                    DisplayTerminalText(partialSentence);
+                    lastCharIndex = nowCharIndex;
+                }
+            }
+        }
+    }
+
+    private void DisplayTerminalText(string sentence) 
+    {
+        TerminalText.text = sentence;
+    }
+
+    public void SetFitnessSentences(int index, string sentence) 
+    {
+        this.setNum = index;
+        this.fitnessSentences[setNum] = sentence;
+        if (! isWriting) 
+        {
+            SetNextSentence(fitnessSentences, writingNum+1);
+        }
+    }
+
+    private void SetNextSentence(string[] sentences, int index) 
+    {
+        nowSentence = sentences[index];
+        beginTime   = Time.time;
+        displayLength = nowSentence.Length * charDisplayInterval;
+        lastCharIndex = 0;
+        isWriting = true;
+
+        // When the text is full with sentences, it is time to scroll up text to display the rest
+        if (writingNum >= maxLinesNumOnScreen) 
+        {
+            isTextScrollUp = true;
+        } 
+    }
+
+    public void InitSetting() 
+    {
+        textObject.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        isTextScrollUp = false;
+        isWriting = false;
+        writingNum = -1;
+        setNum   = -1;
+        wholeSentence = $"第{Simulation.curGeneration}世代\n";
+        DisplayTerminalText(wholeSentence);
+    }
+}
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -18,9 +18,8 @@ public class Simulation : MonoBehaviour
     public GameObject FirePos;
     public GameObject Prefab;
     public GameObject TowerTop;
-    public GameObject FitnessTextObject;
 
-    private UIManager UIManager;
+    private RealWorldUI realWorldUI;
     private Population population;
     private AudioSource audioSource;
     private Animation anim;
@@ -29,39 +28,15 @@ public class Simulation : MonoBehaviour
     private float nextFire;
     [SerializeField][Range(0.1f, 0.5f)] private float fireRate = 0.25f;
 
-    //----- variables for character animation -----//
-    [SerializeField][Range(0.001f, 0.3f)] private float charDisplayInterval = 0.05f; // interval to dispaly next character
-    private bool isActiveUI; // make this true when displaying UI
-    private bool isTextScrollUp; // FitnessTextObject has to move upward to show all texts
-    private string[] sentences;
-    private string wholeSentence;
-    private string nowSentence; // one line sentence
-    private int lastCharIndex; // index for nowSentence
-    private float beginTime;
-    private float displayLength;
-    private int fontSize;
-    private int maxLinesNumOnScreen; // maximum number of lines which the scene can display
-
+   
     private void Awake() 
     {
         audioSource = this.GetComponent<AudioSource>();
         anim = this.GetComponent<Animation>();
-        UIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        realWorldUI = GameObject.Find("Canvas").GetComponent<RealWorldUI>();
         nextFire = fireRate;
-        sentences = new string[BulletNum];
-        isActiveUI = false;
 
         population = new Population(Vector3.Angle(Vector3.right, FirePos.transform.right));
-
-        if (SimulationMode == MODE.NormalSimulation) 
-        {
-            UIManager.SimulationUI();
-        } 
-        else 
-        {
-            fontSize = UIManager.FitnessText.fontSize;
-            maxLinesNumOnScreen = (int)(Screen.height / fontSize);
-        }
 
         // shoot the first bullet
         InitFire();
@@ -69,54 +44,21 @@ public class Simulation : MonoBehaviour
 
     private void Update() 
     {
-        if(isActiveUI) 
+        if (realWorldUI.writingNum == BulletNum-1)
         {
-            // scroll up when the text is full on the screen
-            if (isTextScrollUp) 
-            {
-                // calculate how much the text should be scrolled per frame
-                float scroll = (Time.deltaTime * fontSize) / displayLength; 
-                // scroll *= 1.005f; // an adjustment
-                FitnessTextObject.GetComponent<RectTransform>().localPosition += Vector3.up * scroll;
-            }
-
-            if (Time.time > beginTime + displayLength) 
-            {
-                // time to fire next bullet
-                wholeSentence += nowSentence+"\n";
-                UIManager.DisplayFitnessText(wholeSentence);
-                isActiveUI = false;
-
-                // time to evolve if we reach the bulletNum
-                if (deadCount == BulletNum)
-                {
-                    Evolution();
-                }
-            }
-            else
-            {
-                // here is just visiting visiting nowSentence one by one
-                int nowCharIndex = (int)(Mathf.Clamp01((Time.time-beginTime) / displayLength) * nowSentence.Length);
-                // if index has changed, show it
-                if (nowCharIndex != lastCharIndex)
-                {
-                    string partialSentence = wholeSentence + nowSentence.Substring(0, nowCharIndex);
-                    UIManager.DisplayFitnessText(partialSentence);
-                    lastCharIndex = nowCharIndex;
-                }
-            }
+            Evolution();
         }
-        else 
+
+        if (deadCount < BulletNum) 
         {
             // when current bullet stops running, fire the next one
             if (population.bulletObjects[deadCount].isStopRunning) 
             {
                 // calculate fitness and display on the screen
                 population.curIndividuals[deadCount].fitness = population.bulletObjects[deadCount].calculateFitness();
-                sentences[deadCount] = $"{deadCount+1}: {population.curIndividuals[deadCount].fitness}";
-                SetNextSentence(deadCount);
+                string sentence = $"{deadCount+1}: {population.curIndividuals[deadCount].fitness}";
+                realWorldUI.SetFitnessSentences(deadCount, sentence);
                 deadCount++;
-                isActiveUI = true;
             }
         }
         
@@ -129,8 +71,6 @@ public class Simulation : MonoBehaviour
 
     private void Evolution() 
     {
-        // selectionSentences = population.alternate();
-        // population
         population.alternate();
         
         // finish if we reach the maximum generation count
@@ -162,17 +102,13 @@ public class Simulation : MonoBehaviour
         TowerTop.transform.rotation = Quaternion.Lerp(TowerTop.transform.rotation, towerTopHeading, 1 - (nextFire-Time.time));
     }
 
-    
     private void InitFire() 
     {
-        FitnessTextObject.GetComponent<RectTransform>().localPosition = Vector3.zero;
-        wholeSentence = $"第{curGeneration}世代\n";
-        UIManager.DisplayFitnessText(wholeSentence);
+        realWorldUI.InitSetting();
         deadCount = 0;
         bornCount = 0;
         Fire();
     }
-
     private void Fire() 
     {
         ++bornCount;
@@ -187,26 +123,5 @@ public class Simulation : MonoBehaviour
         population.bulletObjects[bornCount-1] = Instantiate(Prefab, FirePos.transform.position, Quaternion.Euler(-10f, 0f, 0f)).GetComponent<Bullet>();
         // pass the path which this bullet will follow
         population.bulletObjects[bornCount-1].Fire(nowBulletGenom, bornCount);
-    }
-
-    private void SetNextSentence(int nowSentenceNum) 
-    {
-        if (nowSentenceNum == sentences.Length)
-        {
-            // visited all sentences, so back to shooting and go to next generation
-            isTextScrollUp = false;
-            UIManager.DisplayFitnessText(wholeSentence);
-            return;
-        }
-        nowSentence = sentences[nowSentenceNum];
-        beginTime   = Time.time;
-        displayLength = nowSentence.Length * charDisplayInterval;
-        lastCharIndex = 0;
-
-        // When the text is full with sentences, it is time to scroll up text to display the rest
-        if (nowSentenceNum >= maxLinesNumOnScreen) 
-        {
-            isTextScrollUp = true;
-        }
     }
 }
